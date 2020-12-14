e0683a02df6dab934ec189e0f3b64d1b1b65189d
Piumi <aluviharepiumee@gmail.com>
2020-12-14 19:53:09 +0530

    change browserManager

--- src/test/java/com/pragmatic/selenium/hrm/BrowserManager.java
+++ src/test/java/com/pragmatic/selenium/hrm/BrowserManager.java
@@ -19,24 +19,29 @@ public class BrowserManager {
     public static void setupDrivers(String browser_type) {
 
 
-        if (browser_type.isEmpty() || browser_type.isBlank()) {
+        if (browser_type.isEmpty()) {
             System.exit(-1);
         }
 
         switch (browser_type.toLowerCase()) {
-            case "chrome" , "google-chrome", "chrome-headless" -> {
+            case "chrome" :{
                 WebDriverManager.chromedriver().setup();
-            } case  "firefox", "firefox-headless" -> {
+                break;
+            } case  "firefox" : {
                 WebDriverManager.firefoxdriver().setup();
-            }case  "edge" -> {
+                break;
+            } case  "edge" : {
                 WebDriverManager.edgedriver().setup();
-            }case  "ie" -> {
+                break;
+            } case  "ie" : {
                 WebDriverManager.iedriver().setup();
-            }case  "safari" -> {
-                //NOT REQUIRED
-            }case  "opera" -> {
+                break;
+            } case  "safari":{
+                ///WebDriverManager.safari().setup();
+            } case  "opera" : {
                 WebDriverManager.operadriver().setup();
-            }default -> {
+                break;
+            } default : {
                 System.exit(-1);
             }
 
@@ -48,35 +53,48 @@ public class BrowserManager {
     }
 
     public static WebDriver launchBrowser(String browser_type) {
+
+
         WebDriver driver = null;
 
         switch (browser_type.toLowerCase()) {
-            case "chrome" , "google-chrome" -> {
+            case "chrome" : {
                 ChromeOptions options = new ChromeOptions();
+
+
                 options.setExperimentalOption("useAutomationExtension", false);
                 options.setExperimentalOption("excludeSwitches",
                         Collections.singletonList("enable-automation"));
                 driver = new ChromeDriver(options);
-            }case "chrome-headless"  -> {
+
+            } case "chrome-headless" : {
+
                 ChromeOptions options = new ChromeOptions();
                 options.setHeadless(true);
                 driver = new ChromeDriver(options);
-            } case  "firefox" -> {
+
+
+            } case  "firefox" : {
                 driver = new FirefoxDriver();
 
-            }case  "firefox-headless" -> {
+            } case  "firefox-headless" : {
                 FirefoxOptions options = new FirefoxOptions();
                 options.setHeadless(true);
                 driver = new FirefoxDriver(options);
-            }case  "edge" -> {
+
+            } case  "edge" :{
                 driver = new EdgeDriver();
-            }case  "ie" -> {
+
+            } case  "ie" :{
                 driver = new InternetExplorerDriver();
-            }case  "safari" -> {
+
+            } case  "safari" : {
                 driver = new SafariDriver();
-            }case  "opera" -> {
+
+            } case  "opera": {
                 driver = new OperaDriver();
-            }default -> {
+
+            } default: {
                 System.exit(-1);
             }
 
