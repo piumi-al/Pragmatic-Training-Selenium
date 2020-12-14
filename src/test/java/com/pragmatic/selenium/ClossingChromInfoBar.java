1f5a44e60bb5b2c08ded4c730911a494ac022e7d
Piumi <aluviharepiumee@gmail.com>
2020-12-14 10:50:28 +0530

    closing ChromeInfo

---
+++ src/test/java/com/pragmatic/selenium/ClossingChromInfoBar.java
@@ -0,0 +1,33 @@
+package com.pragmatic.selenium;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.Collections;
+
+public class ClossingChromInfoBar {
+
+
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+
+        //Create an instance of Chrome browser browser
+        ChromeOptions options = new ChromeOptions();
+        options.setExperimentalOption("useAutomationExtension", false);
+        options.setExperimentalOption("excludeSwitches",
+                Collections.singletonList("enable-automation"));
+
+        WebDriver driver = new ChromeDriver(options);
+
+
+        //Navigate to the login page
+        driver.get("http://hrm.pragmatictestlabs.com/");
+
+
+
+
+    }
+}
